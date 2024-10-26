34c49ca2d2ab6d6795cc5b746d284185c6bfdc38
MalcolmDahling <mac.dahling@gmail.com>
2024-10-26 23:55:06 +0200

    vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,10 @@ import Wrapper from '@/components/Wrapper/Wrapper';
 
 export default async function Index() {
   console.log('Vercel URL:', process.env.VERCEL_URL);
+  const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
+  const res = await fetch(`${baseUrl}/api/storyblok`);
+  const data = await res.json();
+  console.log(data);
 
   return (
     <Wrapper>
