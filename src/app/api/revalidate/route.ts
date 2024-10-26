29f0ec7da21435d7dcf157cc05d42e6dd30b98a1
MalcolmDahling <mac.dahling@gmail.com>
2024-10-27 00:45:40 +0200

    revalidate

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -4,12 +4,7 @@ import { NextResponse } from 'next/server';
 export async function POST(req: Request) {
   console.log(req);
 
-  try {
-    revalidatePath('/');
+  revalidatePath('/');
 
-    return NextResponse.json({ revalidated: true }, { status: 200 });
-  } catch (err) {
-    console.error('Error revalidating:', err);
-    return NextResponse.json({ error: 'Error revalidating' }, { status: 500 });
-  }
+  return NextResponse.json({ revalidated: true }, { status: 200 });
 }
