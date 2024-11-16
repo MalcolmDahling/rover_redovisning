17313d68ce7c7038c4822ac3a7b6445d04805c75
MalcolmDahling <mac.dahling@gmail.com>
2024-11-16 14:59:21 +0100

    separate fetches for head and page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,180 @@
 import '@/styles/global.css';
+import { StoryblokType } from '@/types/storyblok';
+import React from 'react';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/api/storyblok' : `${process.env.URL}/api/storyblok`;
+  const res = await fetch(url);
+  const data: StoryblokType = await res.json();
+
   return (
     <html lang="sv">
+      <head>
+        <meta
+          name="author"
+          content="Malcolm Dahling and Jonas Sahlström"
+        ></meta>
+        <title>{data.meta_data.title}</title>
+        <meta
+          name="description"
+          content={data.meta_data.description}
+        ></meta>
+        <meta
+          name="keywords"
+          content={data.meta_data.keywords}
+        ></meta>
+        <meta charSet="UTF-8"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+
+        <meta
+          name="theme-color"
+          content="#ffffff"
+        ></meta>
+        <meta
+          name="robots"
+          content="index, follow"
+        ></meta>
+
+        {data.meta_data.canonical_url && (
+          <link
+            rel="canonical"
+            href={data.meta_data.canonical_url}
+          ></link>
+        )}
+
+        <link
+          rel="preconnect"
+          href="https://api.storyblok.com/v2/cdn/"
+        ></link>
+
+        <meta
+          property="og:title"
+          content={data.meta_data.title}
+        ></meta>
+        <meta
+          property="og:description"
+          content={data.meta_data.description}
+        ></meta>
+        <meta
+          property="og:image"
+          content={data.meta_data.canonical_url + '/images/og_image.webp'}
+        ></meta>
+        <meta
+          property="og:image:width"
+          content="1200"
+        ></meta>
+        <meta
+          property="og:image:height"
+          content="630"
+        ></meta>
+        <meta
+          property="og:url"
+          content={data.meta_data.canonical_url}
+        ></meta>
+        <meta
+          property="og:type"
+          content="website"
+        ></meta>
+        <meta
+          property="og:locale"
+          content="sv_SE"
+        ></meta>
+
+        <meta
+          name="twitter:card"
+          content="summary_large_image"
+        ></meta>
+        <meta
+          name="twitter:title"
+          content={data.meta_data.title}
+        ></meta>
+        <meta
+          name="twitter:description"
+          content={data.meta_data.description}
+        ></meta>
+        <meta
+          name="twitter:image"
+          content={data.meta_data.canonical_url + '/images/og_image.webp'}
+        ></meta>
+        <meta
+          name="twitter:text:title"
+          content={data.meta_data.title}
+        ></meta>
+
+        <meta
+          httpEquiv="X-UA-Compatible"
+          content="IE=edge"
+        ></meta>
+        <meta
+          name="referrer"
+          content="no-referrer"
+        ></meta>
+        <meta
+          name="mobile-web-app-capable"
+          content="yes"
+        ></meta>
+        <meta
+          name="apple-mobile-web-app-capable"
+          content="yes"
+        ></meta>
+        <meta
+          name="apple-mobile-web-app-title"
+          content={data.meta_data.title}
+        ></meta>
+
+        <link
+          rel="apple-touch-icon"
+          href={data.meta_data.canonical_url + '/images/apple_touch_icon.png'}
+        ></link>
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        ></meta>
+
+        <meta
+          name="color-scheme"
+          content="light"
+        ></meta>
+
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href={data.favicon.svg_favicon.filename}
+          sizes="any"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href={data.favicon.favicon_16x16.filename}
+          sizes="16x16"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          href={data.favicon.favicon_32x32.filename}
+          sizes="32x32"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          href={data.favicon.favicon_48x48.filename}
+          sizes="48x48"
+        ></link>
+
+        <script type="application/ld+json">
+          {JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'WebPage',
+            name: data.meta_data.title,
+            description: data.meta_data.description,
+            url: data.meta_data.canonical_url,
+          })}
+        </script>
+      </head>
+
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import MenuModal from '@/components/MenuModal/MenuModal';
 import Slider from '@/components/Slider/Slider';
 import Wrapper from '@/components/Wrapper/Wrapper';
 import { StoryblokType } from '@/types/storyblok';
-import Head from 'next/head';
 
 export const revalidate = 60;
 
@@ -16,189 +15,21 @@ export default async function Index() {
   const data: StoryblokType = await res.json();
 
   return (
-    <>
-      <Head>
-        <meta
-          name="author"
-          content="Malcolm Dahling and Jonas Sahlström"
-        ></meta>
-        <title>{data.meta_data.title}</title>
-        <meta
-          name="description"
-          content={data.meta_data.description}
-        ></meta>
-        <meta
-          name="keywords"
-          content={data.meta_data.keywords}
-        ></meta>
-        <meta charSet="UTF-8"></meta>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-
-        <meta
-          name="theme-color"
-          content="#ffffff"
-        ></meta>
-        <meta
-          name="robots"
-          content="index, follow"
-        ></meta>
-
-        {data.meta_data.canonical_url && (
-          <link
-            rel="canonical"
-            href={data.meta_data.canonical_url}
-          ></link>
-        )}
-
-        <link
-          rel="preconnect"
-          href="https://api.storyblok.com/v2/cdn/"
-        ></link>
-
-        <meta
-          property="og:title"
-          content={data.meta_data.title}
-        ></meta>
-        <meta
-          property="og:description"
-          content={data.meta_data.description}
-        ></meta>
-        <meta
-          property="og:image"
-          content={'https://rover-r2.vercel.app/images/og_image.webp'} //ändra till rätt url när sidan är live. måste vara hårdkodad
-        ></meta>
-        <meta
-          property="og:image:width"
-          content="1200"
-        ></meta>
-        <meta
-          property="og:image:height"
-          content="630"
-        ></meta>
-        <meta
-          property="og:url"
-          content={data.meta_data.canonical_url}
-        ></meta>
-        <meta
-          property="og:type"
-          content="website"
-        ></meta>
-        <meta
-          property="og:locale"
-          content="sv_SE"
-        ></meta>
-
-        <meta
-          name="twitter:card"
-          content="summary_large_image"
-        ></meta>
-        <meta
-          name="twitter:title"
-          content={data.meta_data.title}
-        ></meta>
-        <meta
-          name="twitter:description"
-          content={data.meta_data.description}
-        ></meta>
-        <meta
-          name="twitter:image"
-          content={'https://rover-r2.vercel.app/images/og_image.webp'} //ändra till rätt url när sidan är live. måste vara hårdkodad
-        ></meta>
-        <meta
-          name="twitter:text:title"
-          content={data.meta_data.title}
-        ></meta>
-
-        <meta
-          httpEquiv="X-UA-Compatible"
-          content="IE=edge"
-        ></meta>
-        <meta
-          name="referrer"
-          content="no-referrer"
-        ></meta>
-        <meta
-          name="mobile-web-app-capable"
-          content="yes"
-        ></meta>
-        <meta
-          name="apple-mobile-web-app-capable"
-          content="yes"
-        ></meta>
-        <meta
-          name="apple-mobile-web-app-title"
-          content={data.meta_data.title}
-        ></meta>
-
-        <link
-          rel="apple-touch-icon"
-          href={'https://rover-r2.vercel.app/images/apple_touch_icon.png'} //ändra till rätt url när sidan är live. måste vara hårdkodad
-        ></link>
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        ></meta>
-
-        <meta
-          name="color-scheme"
-          content="light"
-        ></meta>
-
-        <link
-          rel="icon"
-          type="image/svg+xml"
-          href={data.favicon.svg_favicon.filename}
-          sizes="any"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          href={data.favicon.favicon_16x16.filename}
-          sizes="16x16"
-        ></link>
-        <link
-          rel="icon"
-          type="image/png"
-          href={data.favicon.favicon_32x32.filename}
-          sizes="32x32"
-        ></link>
-        <link
-          rel="icon"
-          type="image/png"
-          href={data.favicon.favicon_48x48.filename}
-          sizes="48x48"
-        ></link>
-
-        <script type="application/ld+json">
-          {JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'WebPage',
-            name: data.meta_data.title,
-            description: data.meta_data.description,
-            url: data.meta_data.canonical_url,
-          })}
-        </script>
-      </Head>
-
-      <Wrapper>
-        <Header
-          website_name={data.website_name}
-          slides={data.slides}
-          logo={data.logo}
-        ></Header>
-        <Main>
-          <Slider slides={data.slides}></Slider>
-        </Main>
-        <MenuModal slides={data.slides}></MenuModal>
-        <Hamburger></Hamburger>
-        <Footer
-          footer={data.footer}
-          contact_information={data.contact_information}
-        ></Footer>
-      </Wrapper>
-    </>
+    <Wrapper>
+      <Header
+        website_name={data.website_name}
+        slides={data.slides}
+        logo={data.logo}
+      ></Header>
+      <Main>
+        <Slider slides={data.slides}></Slider>
+      </Main>
+      <MenuModal slides={data.slides}></MenuModal>
+      <Hamburger></Hamburger>
+      <Footer
+        footer={data.footer}
+        contact_information={data.contact_information}
+      ></Footer>
+    </Wrapper>
   );
 }
