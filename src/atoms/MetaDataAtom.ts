b4ccddb2732514411b9d32e21dee1ffbc6ad68f5
MalcolmDahling <mac.dahling@gmail.com>
2024-11-16 14:42:41 +0100

    moved head into page

--- src/atoms/MetaDataAtom.ts
+++
@@ -1,9 +0,0 @@
-import { StoryblokType } from '@/types/storyblok';
-import { atom } from 'jotai';
-
-export const MetaDataAtom = atom<StoryblokType['meta_data']>({
-  title: '',
-  description: '',
-  keywords: '',
-  canonical_url: '',
-});
