2956e4d9568cb18f80d07d23bfa2f4559eed29a4
MalcolmDahling <mac.dahling@gmail.com>
2024-11-20 15:34:06 +0100

    facebook 206 error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request: Request) {
-  const response = NextResponse.next();
-  response.headers.set('Accept-Ranges', 'none');
-  return response;
+  const userAgent = request.headers.get('user-agent') || '';
+  if (userAgent.includes('facebookexternalhit')) {
+    const response = NextResponse.next();
+    // Disable caching for Facebook scraper requests
+    response.headers.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+    response.headers.set('Accept-Ranges', 'none');
+    return response;
+  }
+  return NextResponse.next();
 }
